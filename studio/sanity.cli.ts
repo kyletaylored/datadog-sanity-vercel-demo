8c3bd42574177932221e03b592b4a3905efa861a
Kyle Taylor <kyle.taylor@datadoghq.com>
2026-03-25 12:10:19 -0500

    Fix warnings in Studio

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -5,7 +5,7 @@
  * Learn more: https://www.sanity.io/docs/cli
  */
 
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli'
 
 const projectId = process.env.SANITY_STUDIO_PROJECT_ID || '<your project ID>'
 const dataset = process.env.SANITY_STUDIO_DATASET || 'production'
@@ -16,7 +16,7 @@ export default defineCliConfig({
     dataset,
   },
   studioHost: process.env.SANITY_STUDIO_STUDIO_HOST || '', // Visit https://www.sanity.io/docs/studio/environment-variables to learn more about using environment variables for local & production.
-  autoUpdates: true,
+  deployment: { autoUpdates: true },
   typegen: {
     path: './src/**/*.{ts,tsx,js,jsx}',
     schema: '../sanity.schema.json',
